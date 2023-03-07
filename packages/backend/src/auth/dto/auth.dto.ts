7183d9c33dcc44bac32c175b058d3b6cf3f66fdf
Eddy Valverde <eddygarros@hotmail.com>
2023-03-07 14:26:11 -0600

    chore: test

--- packages/backend/src/auth/dto/auth.dto.ts
+++ packages/backend/src/auth/dto/auth.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export interface AuthDto {
-  email: string;
+  @IsEmail()
+  @IsNotEmpty()
+  email string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
