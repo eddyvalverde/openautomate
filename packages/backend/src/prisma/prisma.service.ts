ab90076e9dcab1fd6bff21d505162dd2bede08a9
Eddy Valverde <eddygarros@hotmail.com>
2023-03-19 14:08:25 -0600

    chore: tearDownDatabase when testing

--- packages/backend/src/prisma/prisma.service.ts
+++ packages/backend/src/prisma/prisma.service.ts
@@ -14,4 +14,21 @@ export class PrismaService extends PrismaClient {
       },
     });
   }
+  cleanDb() {
+    return this.$transaction([
+      this.bookmark.deleteMany(),
+      this.dataBaseData.deleteMany(),
+      this.databaseConfiguration.deleteMany(),
+      this.expression.deleteMany(),
+      this.dataBaseStep.deleteMany(),
+      this.step.deleteMany(),
+      this.stepRun.deleteMany(),
+      this.flowRun.deleteMany(),
+      this.email.deleteMany(),
+      this.imapStep.deleteMany(),
+      this.imapConfiguration.deleteMany(),
+      this.flow.deleteMany(),
+      this.user.deleteMany(),
+    ]);
+  }
 }
