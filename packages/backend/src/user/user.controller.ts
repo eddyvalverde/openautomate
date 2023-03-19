db5417cdfe53b047056b59e2231bee4b5e92f605
Eddy Valverde <eddygarros@hotmail.com>
2023-03-18 23:03:37 -0600

    chore: method me return crrent user

--- packages/backend/src/user/user.controller.ts
+++ packages/backend/src/user/user.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { GetUser } from '../auth/decorator/get-user.decorator';
 import { JwtGuard } from '../auth/guard';
 
 @Controller('users')
 export class UserController {
   @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
+  // eslint-disable-next-line prettier/prettier
+  getMe(@GetUser() user: User) { //@GetUser('id') to get only a column value
     /*console.log({
       user: req.user,
     });*/
-    return 'user info';
+    return user;
   }
 }
